c45d4af6efeaa9cc52d8ee233d7e8ec691fa83e9
Seong-Jun1525 <sjsj123455@naver.com>
2025-01-13 17:49:07 +0900

    01-13 오후 공부 내용 - 배열, 함수

--- 01_front-workspace/03_JavaScript/res/js/05_array.js
+++ 01_front-workspace/03_JavaScript/res/js/05_array.js
@@ -80,3 +80,79 @@ function reverseTest() {
         후의 배열상태 : ${arr}
     `
 }
+
+function sortTest() {
+    let arr = ["노승재", "임성준", "양환", "박민준", "정혜영", "김일현"];
+
+    const area4 = document.querySelector("#area4");
+
+    area4.innerHTML = `
+        arr : ${arr}<br>
+        arr : ${arr.sort().reverse()}<br>
+        arr.sort() : ${arr.sort()}<br>
+    `;
+}
+
+function inoutTest1() {
+    let arr = ["삼각동", "입정동", "인사동", "안국동"];
+
+    const area5 = document.getElementById("area5");
+
+    area5.innerHTML = `
+        arr : ${arr}<br>
+        pop() : ${arr.pop()}<br>
+        push() : ${arr.push("행복동")}<br>
+        arr : ${arr}<br>
+    `;
+}
+
+function inoutTest2() {
+    let arr = ["정우성", "조인성", "김혜수", "박신혜", "전도연"];
+
+    const area6 = document.getElementById("area6");
+
+    area6.innerHTML = `
+        arr : ${arr}<br>
+        shift() : ${arr.shift()}<br>
+        unshift() : ${arr.unshift("이정재")}<br>
+        arr : ${arr}<br>
+    `;
+}
+
+function sliceTest() {
+    let arr = ["html", "css", "js", "java", "sql", "python"];
+    const area7 = document.querySelector("#area7");
+
+    area7.innerHTML = `
+        arr : ${arr}<br>
+    `
+    const arr2 = arr.slice(3, 5);
+    area7.innerHTML += `
+        arr2 : ${arr2}<br>
+    `
+
+    area7.innerHTML += `
+        arr : ${arr}<br>
+    `
+
+    let arr3 = arr.splice(0, 3, "spring");
+    area7.innerHTML += `
+        arr3 : ${arr3}<br>
+        arr : ${arr}<br>
+    `
+}
+
+function toStringTest() {
+    let arr = ["저는", "오늘", "빵과", "바나나 우유를", "먹었습니다."];
+
+    const area8 = document.querySelector("#area8");
+
+    // 일반 배열을 출력할 때는 .toString()이 생략되어 있음
+    // => area8.innerHTML = arr; 는 area8.innerHTML = arr.toString()이다.
+
+    area8.innerHTML = `
+        arr: ${arr}<br>
+        arr.toString() : ${arr.toString()}<br>
+        arr.toString() : ${arr.join(" ")}<br>
+    `
+}
---
+++ 01_front-workspace/03_JavaScript/res/js/06_function.js
@@ -0,0 +1,59 @@
+// 함수 선언
+function test1() {
+    document.getElementById("area1").innerHTML += "test1() 함수 호출<br>";
+}
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+
+// 이벤트 속성(onclick)에 익명함수 대입
+btn1.onclick = function() {
+    document.querySelector("#area2").innerHTML += "이벤트 핸들러 실행!<br>"
+}
+
+btn2.onclick = function() {
+    const area2 = document.querySelector("#area2");
+
+    area2.style.backgroundColor = area2.style.backgroundColor == "yellow" ?
+                                    "purple" :
+                                    "yellow";
+
+    // area2.style.backgroundColor = `
+    //     rgb(${Math.floor(Math.random() * 256)},
+    //     ${Math.floor(Math.random() * 256)},
+    //     ${Math.floor(Math.random() * 256)})
+    // `;
+    // area2.style.color = `
+    //     rgb(${Math.floor(Math.random() * 256)},
+    //     ${Math.floor(Math.random() * 256)},
+    //     ${Math.floor(Math.random() * 256)})
+    // `;
+}
+
+// 버튼이 클릭된것처럼 동작시키고자 할 때
+btn2.click();
+
+function test2(value) {
+    document.getElementById("area3").innerHTML += value + "<br>"
+}
+
+function test3() {
+    console.log(arguments);
+    console.log(arguments[2]);
+
+    let sum = 0;
+
+    for(let a of arguments) {
+        sum += a;
+    }
+
+    document.getElementById("area4").innerHTML = "총합 : " + sum;
+}
+
+function test4(target) {
+    // => id 속성으로 접근한 요소 객체와 같은 정보를 전달받을 수 있음
+    target.value = "클릭됨!";
+    target.innerText = "클릭됨@";
+
+    target.style.backgroundColor = "yellow";
+}
