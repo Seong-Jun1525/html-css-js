fc47c7c75d3afed66abc0dcef3cd18bbb9da7117
Seong-Jun1525 <sjsj123455@naver.com>
2025-01-13 11:01:32 +0900

    지난주 공부내용 복습2

--- work/work4/practice1/practice1.js
+++ work/work4/practice1/practice1.js
@@ -21,8 +21,17 @@ function checkBoxTrigger() {
     let hobbyList = document.getElementsByName("hobby");
 
     for(let h of hobbyList) {
+        // input의 value값 영어 -> 한국어로 변경한 부분 반영안했음.. 이 부분 수정
+        // 항상 검토를 잘하자!
         if(h.value === "전체선택") continue;
-        if(!h.checked) allChecked = false;
+        if(!h.checked) {
+            allChecked = false;
+
+            // ★ 검사해야할 항목이 많지만
+            // 이미 false가 되었으면 뒤에 항목은 검사할 필요가 없음
+            // 효율적인 코드를 작성하도록 하자
+            break;
+        }
     }
 
     console.log(allChecked);
@@ -35,19 +44,31 @@ function selectCategoryBtn() {
     
     // 취미 요소
     const hobbyList = document.getElementsByName("hobby");
-    console.log(hobbyList);
+    // console.log(hobbyList);
 
     // 카테고리 출력 박스
     const categoryBox = document.querySelector("#categoryBox");
     // console.log(categoryBox);
     categoryBox.innerHTML = "";
 
+    // for(let h of hobbyList) {
+    //     // "전체 선택"의 텍스트는 출력하는게 아니였음.. 이 부분 수정
+    //     // 다음부턴 문제를 잘 읽자..
+    //     if(h.value === "전체선택") continue; // 이 부분 추가
 
-    for(let h of hobbyList) {
-        if(h.checked) checkedHobbyList.push(h);
-    }
+    //     if(h.checked) checkedHobbyList.push(h.value);
+    // }
+    // console.log(checkedHobbyList);
 
-    for(let i of checkedHobbyList) {
-        categoryBox.innerHTML += i.value + "&nbsp;";
+    // 크롬을 제외한 대부분의 브라우저에서 push() 메소드를 사용해 데이터를 할당하는 방법보다
+    // 접근자 []를 사용해 데이터를 할당하는 코드의 실행 속도가 2배 정도 더 빠름.
+    for(let i = 0; i < hobbyList.length; i++) {
+        if(hobbyList[i].value === "전체선택") continue; // 이 부분 추가
+        if(hobbyList[i].checked) checkedHobbyList.push(hobbyList[i].value);
     }
+    categoryBox.innerHTML = checkedHobbyList.toString();
+
+    // for(let i of checkedHobbyList) {
+    //     categoryBox.innerHTML += i.value + "&nbsp;";
+    // }
 }
